586db60292a10858daad67026a8e219a34e5399b
QhamaFasi <qhama.fasi1@yahoo.com>
2025-07-15 10:59:49 +0200

    Add files via upload

---
+++ Playback Speed.py
@@ -0,0 +1,12 @@
+# Playback Speed
+
+#playback.py
+
+#Prompt the user for input
+user_input = input("Enter your message: ")
+
+#Replace each space with "..."
+slow_playback = user_input.replace(" ", "...")
+
+#Output the slow_playback message
+print(slow_playback)
