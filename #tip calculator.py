9f0aceb98b1dc5e8f26a2e0fa2702546bfdc6f0e
QhamaFasi <qhama.fasi1@yahoo.com>
2025-07-15 11:10:41 +0200

    Add files via upload

---
+++ #tip calculator.py
@@ -0,0 +1,19 @@
+#tip.py
+
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+def dollars_to_float(d):
+    # Remove the dollar sign and convert to float
+    amount = d.replace("$", "")
+    return float(amount)
+
+def percent_to_float(p):
+    # Remove the percent sign and convert to decimal
+    percent = p.replace("%", "")
+    return float(percent) / 100
+
+main()
